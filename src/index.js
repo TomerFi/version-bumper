990bd22ffe7ab32a64d3cfcd795240aab14e95e5
Tomer Figenblat <tomer@tomfi.info>
2024-02-06 21:17:27 -0500

    fix: fixed support for default values in module entering point
    Signed-off-by: Tomer Figenblat <tomer@tomfi.info>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 /**
  * Entrypoint for modules access to the bumper function.
  *
- * @param {string} source Source for the bump, any semver string or 'git' to fetch from tags. Defaults to 'git'.
- * @param {string} path When source is 'git', path of the git repository. Defaults to './'.
- * @param {string} bump Target bump, 'major' | 'minor' | 'patch' | 'auto'. Defaults to 'auto' which can only be used with a 'git' source.
- * @param {string} label Development iteration build label. Defaults to '-dev'.
- * @param {string} preset Conventional preset to use. Defaults to 'angular'.
+ * source: Source for the bump, any semver string or 'git' to fetch from tags. Defaults to 'git'.
+ * path: When source is 'git', path of the git repository. Defaults to './'.
+ * bump: Target bump, 'major' | 'minor' | 'patch' | 'auto'. Defaults to 'auto' which can only be used with a 'git' source.
+ * label: Development iteration build label. Defaults to '-dev'.
+ * preset: Conventional preset to use. Defaults to 'angular'.
+ *
+ * @param {{source: string, path: string, bump: string, label: string, preset: string}} opts options to configure bumper
  * @returns {Promise<{current: string, bump: string, next: string, dev: string}>}
  */
-module.exports = async function (source = 'git', path = './', bump = 'auto', label = '-dev', preset = 'angular') {
-  return require('./bumper')({source, path, bump, label, preset})
+module.exports = async function (opts) {
+  return require('./bumper')({source: 'git', path: './', bump: 'auto', label: '-dev', preset: 'angular', ...opts})
 }
