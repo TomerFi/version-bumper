3bd9df0507adae2b6b8b4da0fc336eabb6824bb1
Tomer Figenblat <tomer@tomfi.info>
2024-08-08 09:53:27 -0400

    fix: broken export in module entrypoint
    Signed-off-by: Tomer Figenblat <tomer@tomfi.info>

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@
  * @param {{source: string, path: string, bump: string, label: string}} opts options to configure bumper
  * @returns {Promise<{current: string, bump: string, next: string, dev: string}>}
  */
-import { bumper } from "./bumper.js"
+import { bumper as bmpr } from "./bumper.js"
 
-export default async function (opts) {
-  return bumper({source: 'git', path: './', bump: 'auto', label: '-dev', ...opts})
+export async function bumper (opts) {
+  return bmpr({source: 'git', path: './', bump: 'auto', label: '-dev', ...opts})
 }
